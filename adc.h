84aca1858e32db1301380cbadeb0b95efaf43edd
jpdelopes <joaopedrodelopes@gmail.com>
2018-03-26 00:46:11 +0100

    initADC added

---
+++ adc.h
@@ -0,0 +1,1 @@
+#define ADC_POT 5
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <p24fxxxx.h>
 #include <stdio.h>
-#include <string.h>
-#include "system_config/io_mapping.h"
+#include "io_mapping.h"
+#include "adc.h"
+
 
 // Configuration Bits
 #ifdef __PIC24FJ64GA004__ //Defined by MPLAB when using 24FJ64GA004 device
@@ -12,80 +13,46 @@ _CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF & COE_OFF & FWDTEN_OFF & ICS_PGx2)
 _CONFIG2( FCKSM_CSDCMD & OSCIOFNC_OFF & POSCMOD_HS & FNOSC_PRI)
 #endif
 
+#define _XTAL_FREQ 	8000000
+#define POT			0x0005
+
+char busyADC()
+{
+	return !(AD1CON1bits.DONE);	
+}
+
+void initADC(){
+/*
+c) Select the analog conversion clock to
+match the desired data rate with the
+processor clock (AD1CON3<7:0>).
+d) Select the appropriate sample/conversion
+sequence (AD1CON1<7:5> and
+AD1CON3<12:8>).
+e) Select how conversion results are
+presented in the buffer (AD1CON1<9:8>).
+f) Select the interrupt rate (AD1CON2<5:2>).
+g) Turn on the A/D module (AD1CON1<15>).
+*/
+	AD1PCFGbits.PCFG5 = 0;			//Analog input AN5
+
+	AD1CON2 = 0x0000;				//Vdd, Vss as Vref+, Vref-
+	AD1CON3 = 0x0001;				//Manual Sample, Tad = 2 Tcy
+	AD1CON1 = 0x0000;				//SAMP bit = 0 ends sampling, starts converting
+	
+    AD1CHS  = POT;
+	
+	AD1CSSL = 0;					//No scanned inputs
+	AD1CON1bits.ADON = 1; // turn ADC ON
+}
 
 int main(void)
 {
+	OSCCON = 0x76; // 8MHz Oscillator
+	
+	initADC();
+	//BusyADC10();
 
-LED0_TRIS = 0;
-LED1_TRIS = 0;
-LED2_TRIS = 0;
-LED3_TRIS = 0;
-LED4_TRIS = 0;
-LED5_TRIS = 0;
-LED6_TRIS = 0;
-
-
-
-	int i = 0;
-
-	while ( 1 ){
-		if ( !PORTDbits.RD7 ){
-			LED0_IO= 1;
-			for( i = 0 ; i < 1000 ; i++){};
-			LED1_IO= 1;
-			for( i = 0 ; i < 1000 ; i++){};
-			LED0_IO= 0;
-			for( i = 0 ; i < 20000 ; i++){};
-			LED2_IO= 1;
-			for( i = 0 ; i < 1000 ; i++){};
-			LED1_IO= 0;
-			for( i = 0 ; i < 1000 ; i++){};
-			LED3_IO= 1;
-			for( i = 0 ; i < 20000 ; i++){};
-			LED2_IO= 0;
-			for( i = 0 ; i < 1000 ; i++){};
-			LED4_IO= 1;
-			for( i = 0 ; i < 1000 ; i++){};
-			LED3_IO= 0;
-			for( i = 0 ; i < 20000 ; i++){};
-			LED5_IO= 1;
-			for( i = 0 ; i < 1000 ; i++){};
-			LED4_IO= 0;
-			for( i = 0 ; i < 20000 ; i++){};
-			LED5_IO= 0;
-			for( i = 0 ; i < 1000 ; i++){};
-			LED6_IO= 1;
-			for( i = 0 ; i < 20000 ; i++){};
-			LED5_IO= 1;
-			for( i = 0 ; i < 20000 ; i++){};
-			LED6_IO= 0;
-			LED4_IO= 1;
-LED5_IO= 0;
-			for( i = 0 ; i < 1000 ; i++){};
-LED3_IO= 1;
-			for( i = 0 ; i < 20000; i++){};
-LED4_IO= 0;
-			for( i = 0 ; i < 1000 ; i++){};
-LED2_IO= 1;
-			for( i = 0 ; i < 20000 ; i++){};
-LED3_IO= 0;
-			for( i = 0 ; i < 1000 ; i++){};
-LED1_IO= 1;
-			for( i = 0 ; i < 20000; i++){};
-LED0_IO= 1;
-			for( i = 0 ; i < 1000 ; i++){};
-LED0_IO= 0;
-			for( i = 0 ; i < 20000; i++){};
-LED2_IO= 0;
-			for( i = 0 ; i < 20000; i++){};
-LED1_IO= 0;
-			for( i = 0 ; i < 20000; i++){};
-LED0_IO= 0;
-			for( i = 0 ; i < 1000 ; i++){};
-
-
-
-
-		}
-	}
+	return 0;
 }
+
