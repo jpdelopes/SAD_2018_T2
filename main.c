2e93a8c954c160216530a8a581524c97392a44eb
jpdelopes <joaopedrodelopes@gmail.com>
2018-03-26 11:58:08 +0100

    added stuff

--- main.c
+++ main.c
@@ -12,6 +12,30 @@ _CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF & COE_OFF & FWDTEN_OFF & ICS_PGx2)
 _CONFIG2( FCKSM_CSDCMD & OSCIOFNC_OFF & POSCMOD_HS & FNOSC_PRI)
 #endif
 
+#define BAUDRATE 		25
+#define UART_TX_TRIS 	TRISFbits.TRISF5
+#define UART_RX_TRIS 	TRISFbits.TRISF4
+
+void initUART(void)	
+{
+	// Set directions of UART IOs
+	UART_TX_TRIS = 0;
+	UART_RX_TRIS = 1;
+	U2BRG = BAUDRATE;
+	U2MODE = 0;				//8-bit, no parity, 1 stop bit
+	U2STA = 0;
+	U2MODEbits.UARTEN = 1;	//Enable module
+	U2STAbits.UTXEN = 1;	//Enable transmission
+  	// reset RX flag
+ 	IFS1bits.U2RXIF = 0;
+}
+
+void  putCharUART(char Ch){
+    // wait for empty buffer  
+    while(U2STAbits.UTXBF == 1);
+      U2TXREG = Ch;
+}
+
 int main(void)
 {
 	//OSCCON = 0x76; // 8MHz Oscillator
